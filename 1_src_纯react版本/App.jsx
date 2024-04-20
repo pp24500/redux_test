4196f98ffee2c2013a9c51689a1fe2c43f2beffd
pp24500 <cheng_zi_yu@163.com>
2024-04-20 21:26:32 +0800

    纯react版本

---
+++ 1_src_纯react版本/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Count from './components/Count'
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Count />
+      </div>
+    )
+  }
+}
+
